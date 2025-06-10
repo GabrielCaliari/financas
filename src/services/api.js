44f39fac2d1122dfe762be525f8ff8e9cf10183b
Gabriel Caliari <gabriel15caliari@hotmail.com>
2025-06-10 15:45:11 -0300

    feat: atualizando o app para subir na google Play novamente

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://backendfinancas-production-a109.up.railway.app',
+  baseURL: 'http://10.0.2.2:8080',
 });
 
 export default api;
